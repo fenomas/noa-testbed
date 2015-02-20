20cca0afcc44655aea9685ac36ce3dfe9c2cb4ed
andyhall <andy@fenomas.com>
2015-02-20 19:05:37 +0900

    rearranging stuff

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
 
 var noa = require('noa')
-//var texturePath = require('painterly-textures');
-
-// pass in a more interesting generator function
-var worldGenerator = require('./worldgen');
-
 
 var opts = {
   pointerLock: true,
   inverseY: true,
   chunkSize: 32,
   texturePath: 'painterly/',
-  generator: worldGenerator,
+  generator: require('./worldgen'), // pass in a more interesting generator function
   chunkAddDistance: 3,
   chunkRemoveDistance: 4,
   // movement
@@ -26,6 +21,7 @@ var opts = {
   rotationScale: 0.0025
 }
 
+// create engine
 var game = noa( opts )
 
 
