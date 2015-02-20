c5d93017c181095bb9bb64931a1649d1b3a608f7
andyhall <andy@fenomas.com>
2015-02-20 15:50:15 +0900

    Move block/material definitions into client app, to work with new registry

--- index.js
+++ index.js
@@ -29,3 +29,26 @@ var opts = {
 var game = noa( opts )
 
 
+// define block types and textures. TODO: fit these into options object?
+
+var reg = game.registry
+// materials
+reg.defineMaterial( 1, [1,1,1], "dirt.png" )
+reg.defineMaterial( 2, [1,1,1], "grass.png" )
+reg.defineMaterial( 3, [1,1,1], "grass_dirt.png" )
+reg.defineMaterial( 4, [1,1,1], "cobblestone.png" )
+for (i=5; i<30; i++) {
+  reg.defineMaterial( i, [ Math.random(), Math.random(), Math.random() ], null )
+}
+// block types
+reg.defineBlock( 1, 1 )             // dirt
+reg.defineBlock( 2, [3,3,2,1,3,3] ) // grass
+reg.defineBlock( 3, 4 )             // stone
+for (var i=4; i<30; i++) {          // random colors
+  reg.defineBlock( i, i+1 )
+}
+
+
+
+
+
